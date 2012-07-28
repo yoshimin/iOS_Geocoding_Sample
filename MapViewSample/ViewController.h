dc3d1259910411a34c394c40b881b928854fb2fa
yoshimin <yoshimi@tonchidot.com>
2012-07-28 18:38:24 +0900

    Initial Commit

---
+++ MapViewSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapViewSample
+//
+//  Created by Yoshimi Shingai on 7/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
